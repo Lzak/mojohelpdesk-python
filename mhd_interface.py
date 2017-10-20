08ee99f50074aea77574e25d38c287e612fde377
Lasha Zakariashvili <Lzak95@ymail.com>
2017-10-20 12:50:52 -0400

    Added modular features to mhd_interface.py

--- mhd_interface.py
+++ mhd_interface.py
@@ -6,30 +6,74 @@
 
 	Example Object:
 	{
-		'location': 
-		'subject':
+		'company_id':
+		'custom_fields': {
+			'abc':'123'
+		},
 		'description':
-		'tixType':
+		'priority_id':
+		'subject':
+		'title': 
+		'user_id':
 	}
 
 
 '''
 
 import requests
+from lxml import etree
 
-def createTicket(obj):
+#URL example:  company.mojohelpdesk.com
+class MHD():
+	def __init__(self, url, apikey):
+		self.baseurl = "http://" + url + "/api/"
+		self.apikey = apikey 
 
-	headers = {
-		'Accept': 'application/xml',
-		'Content-type': 'application/xml',
-	}
+	def createTicket(self, obj):
+		url = self.baseurl + "ticket"
 
-	data = "<ticket><title>" + obj['location'] + " - " + obj['subject'] + "</title>" + \
-			"<description>"  + obj['description'] + "</description>" + \
-			"<company_id>"   + obj['company_id'] + "</company_id>" + \
-			"<priority_id>"  + obj['priority'] + "</priority_id>" + \
-			"<user_id>" 	 + obj['user_id'] + "</user_id></ticket>"
+		params = {
+			'access_key': self.apikey
+		}
 
-	r = requests.post(url_ticket, headers=headers, data=data)
-	print "MojoHelpDesk Returned This:"
-	print r.text
+		headers = {
+			'Accept': 'application/xml',
+			'Content-type': 'application/xml',
+		}
+
+		ticket = etree.Element('ticket')
+		etree.SubElement(ticket, 'title').text = obj['title']
+		etree.SubElement(ticket, 'description').text = obj['description']
+		etree.SubElement(ticket, 'company_id').text = obj['company_id']
+		etree.SubElement(ticket, 'priority_id').text = obj['priority_id']
+		if (len(obj['custom_fields']) > 0):
+			for field in obj['custom_fields']:
+				etree.SubElement(ticket, field).text = obj['custom_fields'][field]
+		etree.SubElement(ticket, 'user_id').text = obj['user_id']
+
+		ticket = etree.tostring(ticket)
+
+		print "URL: " + url
+		print "Params: " + str(params)
+		print "Header: " + str(headers)
+		print "Data: " + ticket
+		#r = requests.post(url_ticket, params=params, headers=headers, data=ticket)
+		#print "MojoHelpDesk Returned This:"
+		#print r.text
+
+
+
+obj = {
+	'company_id': '123',
+	'custom_fields': {
+		'cus_clin':'123'
+	},
+	'description':'test',
+	'priority_id':'20',
+	'subject':'okwut',
+	'title':'hmm',
+	'user_id':'123'
+}
+
+test = MHD("hemtech.mojohelpdesk.com", "12345")
+test.createTicket(obj)
